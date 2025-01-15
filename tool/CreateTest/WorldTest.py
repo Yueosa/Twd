805d1a6195a4fb0853335aaed5cf9f2873ff693e
ReverieX <gj39981225@163.com>
2025-01-15 23:28:28 +0800

    修改import路径

--- tool/CreateTest/WorldTest.py
+++ tool/CreateTest/WorldTest.py
@@ -1,6 +1,13 @@
-import Oracles, Weaving
+import sys
+import os
 
+# 添加父目录到系统路径
+sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
 
+import Oracles
+import Weaving
+
+# 主程序
 Rworld, state = Oracles.Reset.Create()
 Tworld = Oracles.Terrain.Create(Rworld)
 print('正在生成洞穴...')
@@ -8,4 +15,4 @@ print(Tworld)
 weave = Weaving.Weave()
 print('正在生成世界...')
 
-weave.Save(Weaving.Save_path(weave), Weaving.Save_image(weave, Tworld))
+weave.Save(Weaving.Save_image(weave, Tworld))
